5e32cd111688c126d9964889e3f66ecfb3caebc3
Tony Guntharp <fusion94@damagestudios.net>
2014-02-28 20:47:07 -0800

    starting to add the framework for unit tests

--- Gemfile
+++ Gemfile
@@ -19,3 +19,15 @@ group :assets do
 end
 
 gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :test do
+  gem 'faker'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'launchy'
+end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ end
 
 module Rims
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
