5f0eef4e37911d09818630db18f5c4ff8fe32f1c
Tony Guntharp <fusion94@damagestudios.net>
2014-03-06 08:22:41 -0800

    added roles Fixes #12

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,15 @@ class Ability
     # Define abilities for the passed in user here.
     user ||= User.new # guest user (not logged in)
     # a signed-in user can do everything
-    if user_signed_in?
+    if user.has_role? :admin
+     # an admin can do everything
       can :manage, :all
+    elsif user.has_role? :editor
+      # an editor can do everything but delete items
+      can [:read, :create, :update], Items
+    elsif user.has_role? :guest
+      # a guest can only read items from database
+      can :read, [Items]
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'role_model'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -5,7 +7,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  include RoleModel
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  # optionally set the integer attribute to store the roles in,
+  # :roles_mask is the default
+  roles_attribute :roles_mask
+
+  # declare the valid roles -- do not change the order if you add more
+  # roles later, always append them at the end!
+  roles :admin, :editor, :guest
+
 end
