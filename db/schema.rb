2e45cf68e9f28cbee44f9f023b30abafc53d2b79
Tony Guntharp <fusion94@damagestudios.net>
2014-03-07 11:14:02 -0800

    adding a real seeds file...

--- db/schema.rb
+++ db/schema.rb
@@ -9,42 +9,42 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130524132945) do
+ActiveRecord::Schema.define(version: 20130524132945) do
 
-  create_table "items", :force => true do |t|
+  create_table "items", force: true do |t|
     t.text     "name"
     t.text     "description"
     t.text     "product_model_number"
     t.string   "part_number"
     t.string   "vendor"
     t.string   "quantity"
-    t.decimal  "unit_value",           :precision => 10, :scale => 2, :default => 0.0
-    t.decimal  "value",                :precision => 10, :scale => 2, :default => 0.0
+    t.decimal  "unit_value",           precision: 10, scale: 2
+    t.decimal  "value",                precision: 10, scale: 2
     t.string   "picture"
     t.text     "url"
     t.string   "location"
-    t.datetime "created_at",                                                           :null => false
-    t.datetime "updated_at",                                                           :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
