25a0ba669a8aa3d6c76f6e3097ee64a26e50676d
Tony Guntharp <fusion94@damagestudios.net>
2014-03-23 10:21:59 -0700

    adding style to buttons on show items

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -20,7 +20,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     "uploads/items/images"
   end
 
