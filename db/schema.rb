b3b37a62a4ded0692093f184f6a999afc4f6b465
Alan Wernick <awernick@miners.utep.edu>
2014-12-31 01:52:30 -0700

    Allow login with username or email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141231074900) do
+ActiveRecord::Schema.define(version: 20141231082205) do
 
   create_table "items", force: :cascade do |t|
     t.text     "name"
@@ -44,11 +44,12 @@ ActiveRecord::Schema.define(version: 20141231074900) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role"
-    t.string   "username"
     t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
