5b36db58aa74eb177972ffe854ce09ff3b5864e6
Alan Wernick <awernick@miners.utep.edu>
2014-12-31 13:23:59 -0700

    Enable search with :name and :description using AJAX

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,20 +2,20 @@ class Item < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   attr_accessible :description, :location, :name, :picture, :product_model_number, :vendor_part_number, :quantity, :unit_value, :vendor_url, :value, :vendor_name, :category
 
-self.per_page = 5
+  self.per_page = 5
 
-belongs_to :item
-after_save :update_value
+  belongs_to :item
+  after_save :update_value
 
-def self.search(search)
-  if search
-    where('name LIKE ? OR description LIKE ?', "%#{search}%")
-  else
-    scoped
+  def self.search(search)
+    if search
+      where('name LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%")
+    else
+      where(nil)
+    end
   end
-end
 
-protected
+  protected
 
   def update_value
    update_column :value, (quantity * unit_value)
