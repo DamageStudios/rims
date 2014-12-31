8b23f11b54f801c0e887c2e61d593cc160a731c5
Tony Guntharp <fusion94@damagestudios.net>
2014-12-31 06:00:00 -0800

    adding totals

---
+++ db/seeds-users.rb
@@ -0,0 +1,3 @@
+User.create!([
+  {email: "admin@example.com", password: "changeme", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-03-10 11:28:22", last_sign_in_at: "2014-03-10 11:28:22", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", role: 2}, {email: "user@example.com", password: "changeme", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-03-10 11:28:22", last_sign_in_at: "2014-03-10 11:28:22", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", role: 0}
+])
