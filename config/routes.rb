451a04c7c4e4b48d85345931087dc48e30e88a9b
Tony Guntharp <fusion94@damagestudios.net>
2017-07-06 18:43:30 -0700

    cleanup

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rims::Application.routes.draw do
-  get "welcome/index"
-  devise_for :users
-
-  root :to => 'welcome#index'
-  #root :to => redirect('/items')
-  resources :items
-  resources :users
-end
